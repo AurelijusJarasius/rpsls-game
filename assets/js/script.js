6d7967305877cde16c4d4fd559aa64f14401fce0
Aurelijus Jarasius <aurelijusjarasius@gmail.com>
2024-06-23 19:42:29 +0000

    add javascript functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,29 @@
+const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 
-document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByTagName("button");
+const rules = {
+    rock: { scissors: 'crushes', lizard: 'crushes' },
+    paper: { rock: 'covers', spock: 'disproves' },
+    scissors: { paper: 'cuts', lizard: 'decapitates' },
+    lizard: { paper: 'eats', spock: 'poisons' },
+    spock: { rock: 'vaporizes', scissors: 'smashes' },
+};
 
-    for (let button of buttons) {
-        button.addEventListener("click", function (){
-            if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit!");
-            } else {
-                let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
-            }
-        })
+function getComputerChoice() {
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+function play(playerChoice) {
+    const computerChoice = getComputerChoice();
+    const resultDiv = document.getElementById('result');
+
+    if (playerChoice === computerChoice) {
+        resultDiv.textContent = `It's a tie! Both chose ${playerChoice}.`;
+    } else if (computerChoice in rules[playerChoice]) {
+        const action = rules[playerChoice][computerChoice];
+        resultDiv.textContent = `You win! ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)} ${action} ${computerChoice}.`;
+    } else {
+        const action = rules[computerChoice][playerChoice];
+        resultDiv.textContent = `You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} ${action} ${playerChoice}.`;
     }
-})
-
-function runGame () {
-
-
 }
-
-function choosePlayer () {
-
-
-}
-
-function checkResult () {
-
-
-}
-
-function incrementWin () {
-
-
-}
-
-function incrementLoss () {
-
-
-}
-
-
